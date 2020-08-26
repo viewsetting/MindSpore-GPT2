06017e7feed6a559fd50eec7379eaca709bba8d2
Silypie <52056975+Silypie@users.noreply.github.com>
2020-08-26 10:29:38 +0800

    model for reading comprehension task

---
+++ src/GPT2ForReadComprehension.py
@@ -0,0 +1,31 @@
+import mindspore.nn as nn
+from mindspore.common.initializer import TruncatedNormal
+from GPT2_model import GPT2Model
+from mindspore.ops import operations as P
+
+
+class GPT2CoQAModel(nn.Cell):
+    '''
+        This class is responsible for SQuAD
+        The returned output represents the final logits as the results of log_softmax is propotional to that of softmax.
+    '''
+
+    def __init__(self, config, is_training, use_one_hot_embeddings=False):
+        super(GPT2CoQAModel, self).__init__()
+        self.gpt2 = GPT2Model(config, is_training, use_one_hot_embeddings)
+        self.weight_init = TruncatedNormal(config.initializer_range)
+        self.dense1 = nn.Dense(config.d_model, config.vocab_size, weight_init=self.weight_init, has_bias=True).to_float(
+            config.compute_type)
+        self.vocab_size = config.vocab_size
+        self.dtype = config.dtype
+        self.log_softmax = P.LogSoftmax(axis=1)
+
+    def construct(self, input_ids, input_mask):
+        decoder_output, _ = self.bert(input_ids, input_mask)
+        batch_size, seq_length, hidden_size = P.Shape()(decoder_output)
+        sequence = P.Reshape()(decoder_output, (-1, hidden_size))
+        logits = self.dense1(sequence)
+        logits = P.Cast()(logits, self.dtype)
+        logits = P.Reshape()(logits, (batch_size, seq_length, self.vocab_size))
+        logits = self.log_softmax(logits)
+        return logits
