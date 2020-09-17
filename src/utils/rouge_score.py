0e7ba7deb59a7b67b233f669f01ad9d44136fe14
viewsetting <viewsetting@gmail.com>
2020-09-17 21:07:03 +0800

    Update rouge_score.py

--- src/utils/rouge_score.py
+++ src/utils/rouge_score.py
@@ -1,8 +1,10 @@
+"""Calculate ROUGE score."""
 from typing import List
 from rouge import Rouge
 
-H_PATH = "summaries.txt"
-R_PATH = "rederences.txt"
+# H_PATH = "summaries.txt"
+# R_PATH = "references.txt"
+
 
 def rouge(hypothesis: List[str], target: List[str]):
     """
@@ -12,31 +14,25 @@ def rouge(hypothesis: List[str], target: List[str]):
         hypothesis (List[str]): Inference result.
         target (List[str]): Reference.
     """
-    def cut(s):
-        idx = s.find("</s>")
-        if idx != -1:
-            s = s[:idx]
-        return s
+
     if not hypothesis or not target:
         raise ValueError(f"`hypothesis` and `target` can not be None.")
 
     edited_hyp = []
     edited_ref = []
-
     for h, r in zip(hypothesis, target):
-        h = cut(h).replace("<s>", "").strip()
-        r = cut(r).replace("<s>", "").strip()
-        edited_hyp.append(h + "\n")
-        edited_ref.append(r + "\n")
+        edited_hyp.append(h)
+        edited_ref.append(r)
 
     _rouge = Rouge()
     scores = _rouge.get_scores(edited_hyp, edited_ref, avg=True)
     print(" | ROUGE Score:")
-    print(f" | R-1(F): {scores['rouge-1']['f'] * 100:8.2f}")
-    print(f" | R-2(F): {scores['rouge-2']['f'] * 100:8.2f}")
-    print(f" | R-L(F): {scores['rouge-l']['f'] * 100:8.2f}")
+    print(f" | RG-1(F): {scores['rouge-1']['f'] * 100:8.2f}")
+    print(f" | RG-2(F): {scores['rouge-2']['f'] * 100:8.2f}")
+    print(f" | RG-L(F): {scores['rouge-l']['f'] * 100:8.2f}")
 
-    with open(H_PATH, "w") as f:
-        f.writelines(edited_hyp)
-    with open(R_PATH, "w") as f:
-        f.writelines(edited_ref)
+    # with open(H_PATH, "w") as f:
+    #     f.writelines(edited_hyp)
+
+    # with open(R_PATH, "w") as f:
+    #     f.writelines(edited_ref)
