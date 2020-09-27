0bf058c32ea43dd0c563b654d61a2d1ea31cf72d
ouyangyangxy <1159941086@qq.com>
2020-09-27 21:30:35 +0800

    Cross Entropy calculate

---
+++ src/utils/CrossEntropy.py
@@ -0,0 +1,34 @@
+from mindspore.common.tensor import Tensor
+from mindspore.common import dtype as mstype
+from mindspore.ops import operations as P
+import mindspore.nn as nn
+
+
+class CrossEntropyCalculation(nn.Cell):
+    """
+    Cross Entropy loss
+    """
+    def __init__(self, is_training=True):
+        super(CrossEntropyCalculation, self).__init__()
+        self.onehot = P.OneHot()
+        self.on_value = Tensor(1.0, mstype.float32)
+        self.off_value = Tensor(0.0, mstype.float32)
+        self.reduce_sum = P.ReduceSum()
+        self.reduce_mean = P.ReduceMean()
+        self.reshape = P.Reshape()
+        self.last_idx = (-1,)
+        self.neg = P.Neg()
+        self.cast = P.Cast()
+        self.is_training = is_training
+        self.print = P.Print()
+
+    def construct(self, logits, label_ids, num_labels): # logits [batch * seq_length, vocab_size]   label_ids [batch, seq_length]
+        if self.is_training:
+            label_ids = self.reshape(label_ids, self.last_idx) # label_ids [batch * seq_length]
+            one_hot_labels = self.onehot(label_ids, num_labels, self.on_value, self.off_value) # [batch * seq_length, vocab_size]
+            per_example_loss = self.neg(self.reduce_sum(one_hot_labels * logits, self.last_idx)) # [batch * seq_length]
+            loss = self.reduce_mean(per_example_loss, self.last_idx) # a number
+            return_value = self.cast(loss, mstype.float32)
+        else:
+            return_value = logits * 1.0 # [batch * seq_length, vocab_size]
+        return return_value
---
+++ src/utils/metric_method.py
@@ -0,0 +1,77 @@
+import math
+import numpy as np
+
+class Accuracy():
+    """
+    calculate accuracy
+    """
+    def __init__(self):
+        self.acc_num = 0
+        self.total_num = 0
+
+    def update(self, logits, labels):
+        labels = labels.asnumpy()
+        labels = labels[:, 1:]
+        # print("Accuracy labels length: {}".format(len(labels[0])))
+        labels = np.reshape(labels, -1)
+        logits = logits.asnumpy()
+        logits_id = np.argmax(logits, axis=-1)
+        self.acc_num += np.sum(labels == logits_id)
+        self.total_num += len(labels)
+        print("=========== accuracy is {} ===========".format(self.acc_num / self.total_num))
+
+def postprocess(backpointers, best_tag_id):
+    '''
+    Do postprocess
+    '''
+    best_tag_id = best_tag_id.asnumpy()
+    batch_size = len(best_tag_id)
+    best_path = []
+    for i in range(batch_size):
+        best_path.append([])
+        best_local_id = best_tag_id[i]
+        best_path[-1].append(best_local_id)
+        for bptrs_t in reversed(backpointers):
+            bptrs_t = bptrs_t[0].asnumpy()
+            local_idx = bptrs_t[i]
+            best_local_id = local_idx[best_local_id]
+            best_path[-1].append(best_local_id)
+        # Pop off the start tag (we dont want to return that to the caller)
+        best_path[-1].pop()
+        best_path[-1].reverse()
+    return best_path
+
+class F1():
+    '''
+    calculate F1 score
+    '''
+    def __init__(self, use_crf=False, num_labels=2):
+        self.TP = 0
+        self.FP = 0
+        self.FN = 0
+        self.use_crf = use_crf
+        self.num_labels = num_labels
+
+    def update(self, logits, labels):
+        '''
+        update F1 score
+        '''
+        labels = labels.asnumpy()
+        labels = np.reshape(labels, -1)
+        if self.use_crf:
+            backpointers, best_tag_id = logits
+            best_path = postprocess(backpointers, best_tag_id)
+            logit_id = []
+            for ele in best_path:
+                logit_id.extend(ele)
+        else:
+            logits = logits.asnumpy()
+            logit_id = np.argmax(logits, axis=-1)
+            logit_id = np.reshape(logit_id, -1)
+        pos_eva = np.isin(logit_id, [i for i in range(1, self.num_labels)])
+        pos_label = np.isin(labels, [i for i in range(1, self.num_labels)])
+        self.TP += np.sum(pos_eva&pos_label)
+        self.FP += np.sum(pos_eva&(~pos_label))
+        self.FN += np.sum((~pos_eva)&pos_label)
+
+
