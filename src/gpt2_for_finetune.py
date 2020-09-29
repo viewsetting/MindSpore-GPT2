8d4439db2d42f62cfde8f6e08a2c8d2e45ed2c3a
viewsetting <viewsetting@gmail.com>
2020-09-29 15:21:04 +0800

    Update GPT2Summarization

--- src/gpt2_for_finetune.py
+++ src/gpt2_for_finetune.py
@@ -315,24 +315,24 @@ class GPT2CoQA(nn.Cell):
 
 class GPT2Summarization(nn.Cell):
     def __init__(self, config, is_training, use_one_hot_embeddings=False):
-        super(GPT2ForSummarization, self).__init__()
+        super(GPT2Summarization, self).__init__()
         self.gpt2 = GPT2ForPredictNext(config, is_training, use_one_hot_embeddings)
         self.is_training = is_training
         self.last_idx = (-1,)
         self.log_softmax = P.LogSoftmax(axis=-1)
         self.reshape = P.Reshape()
         self.shape = P.Shape()
+        self.batch_size = config.batch_size
+        self.seq_length = config.seq_length
         self.loss_function = CrossEntropyCalculation(is_training=self.is_training)
-    def construct(self, input_ids,input_mask):
+    def construct(self, input_ids,input_mask,label_ids):
         output = self.gpt2(input_ids,input_mask)
 
-        pre_lm_logits = lm_logits[:batch_size, :sequence_length-1, ...]
+        pre_lm_logits = output[:self.batch_size, :self.seq_length-1, ::]
 
         shift_squeezed_logits = self.reshape(
             pre_lm_logits, (-1, pre_lm_logits.shape[-1]))
-        shift_squeezed_labels = self.reshape(labels[..., 1:], (-1,))
+        shift_squeezed_labels = self.reshape(label_ids[::, 1:], (-1,))
 
         loss = self.loss_function(shift_squeezed_logits, shift_squeezed_labels)
-
-
         return loss
