39e2969e46f0d208535b9d88993cee4b968e6d72
viewsetting <viewsetting@gmail.com>
2020-10-07 20:47:14 +0800

    Update CrossEntropy.py

--- src/utils/CrossEntropy.py
+++ src/utils/CrossEntropy.py
@@ -26,7 +26,7 @@ class CrossEntropyCalculation(nn.Cell):
     def construct(self, logits, label_ids): # logits [batch * seq_length, vocab_size]   label_ids [batch, seq_length]
         if self.is_training:
             label_ids = self.reshape(label_ids, self.last_idx) # label_ids [batch * seq_length]
-            one_hot_labels = self.onehot(label_ids, num_labels, self.on_value, self.off_value) # [batch * seq_length, vocab_size]
+            one_hot_labels = self.onehot(label_ids, self.num_labels, self.on_value, self.off_value) # [batch * seq_length, vocab_size]
             per_example_loss = self.neg(self.reduce_sum(one_hot_labels * logits, self.last_idx)) # [batch * seq_length]
             loss = self.reduce_mean(per_example_loss, self.last_idx) # a number
             return_value = self.cast(loss, mstype.float32)
