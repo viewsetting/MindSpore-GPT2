ee1ff83162d13cd56048d51256bfb09cfd4042f5
viewsetting <viewsetting@gmail.com>
2020-11-13 18:50:18 +0800

    update evalshell template to accomodate more general usages

--- scripts/eval_template_script.sh
+++ scripts/eval_template_script.sh
@@ -1,37 +1,38 @@
 #activate environment
 source /data/tju/env.sh &&
 
-#task
+#output log file path
+output_log="noBOS_topk2_summary.log"
+
+#task info
 model="GPT2"
 size="small"
 task="Summarization"
 dataset="CNN_Dailymail"
 
-#device
+#device setting
 device="Ascend"
 device_id=7
 
-#run_setting
+#run setting
 do_train=False
 do_eval=True
 epoch_num=1
 eval_load_param_mode='finetune'
 
-#dataset
+#dataset path
 datafile='/data/tju/mindspore-dataset/cnn_dailymail-test-mindrecord'
 
 #sample setting
 topk=2
-append_eos=True
 topp=1.0
 temp=1
 
+#for CNN_DailyMail dataset modified parameter
+append_eos=True
 
-#fintune no BOS loss version
-no_BOS_ckpt="/data/tju/pretrained-weight/summary/gpt2_summarization_noBOS_-2_71778.ckpt"
-
-#output log
-output_log="noBOS_topk2_summary.log"
+#fintune ckpt file path
+fintune_ckpt_path="/data/tju/pretrained-weight/summary/gpt2_summarization_noBOS_-2_71778.ckpt"
 
 #create file and head line
 echo "EVAL LOG FILE" > $output_log
@@ -48,14 +49,14 @@ echo $(date "+%Y-%m-%d %H:%M:%S") >> $output_log
 echo sample params: >> $output_log
 echo top_k $topk top_p $topp temperature $temp>>$output_log
 echo mode: $eval_load_param_mode >> $output_log
-echo ckpt_path: $no_BOS_ckpt>>$output_log
+echo fintune_ckpt_path: $fintune_ckpt_path>>$output_log
 
 
 #run python
 nohup python run_summarization_model.py  --device_target=$device --device_id=$device_id \
     --do_eval=$do_eval --do_train=$do_train --eval_load_param_mode=$eval_load_param_mode --epoch_num=$epoch_num \
     --top_p=$topp --top_k=$topk --temp=$temp   --append_eos=$append_eos \
-    --load_finetune_ckpt_path=$no_BOS_ckpt --eval_data_file_path=$datafile >> $output_log 2>&1 &
+    --load_finetune_ckpt_path=$fintune_ckpt_path --eval_data_file_path=$datafile >> $output_log 2>&1 &
 
 #record end time
 echo "END_TIME" >> $output_log
